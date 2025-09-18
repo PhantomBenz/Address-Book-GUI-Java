3fcd17417469a6b7152ecd009367b58ad435f497
Sumoon Byanjankar <sumoon.dev@gmail.com>
2025-09-18 21:35:25 +0545

    feat: Login Page

--- src/addressbook/App.java
+++ src/addressbook/App.java
@@ -1,50 +1,32 @@
 package addressbook;
 
 import java.io.IOException;
-
 import javafx.application.Application;
-import javafx.scene.Group;
-// import javafx.fxml.FXMLLoader;
-// import javafx.scene.Parent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyCombination;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.Line;
-import javafx.scene.shape.Polygon;
-import javafx.scene.shape.Rectangle;
 
-public class App extends Application {
+public class App extends Application{
+
     @Override
-    public void start(Stage primaryStage) throws IOException {
-
-        /* Just tried some stuff */
+    public void start(Stage stage) throws IOException {
         
-        Stage stage = new Stage();
-        Group root = new Group();
-        Scene scene = new Scene(root, Color.gray(0));
+        Parent root = FXMLLoader.load(getClass().getResource("assets/style/Main.fxml"));
+        Scene scene = new Scene(root);
 
-        stage.setTitle("Phantom Testing");
-        stage.setWidth(470);
-        stage.setHeight(470);
-        stage.setResizable(true);
-        stage.setX(50);
-        stage.setY(50);
-        // stage.setFullScreen(true);
-        // stage.setFullScreenExitHint("Press q to exit");
-        // stage.setFullScreenExitKeyCombination(KeyCombination.valueOf("q"));
-
+        String imagePath = "assets/image/icon.png";
+        
+        Image icon = new Image(getClass().getResourceAsStream(imagePath));
+        stage.getIcons().add(icon);
+        stage.setTitle("Address Book GUI");
         stage.setScene(scene);
         stage.show();
+
     }
 
     public static void main(String[] args) {
         launch(args);
     }
-
 }
